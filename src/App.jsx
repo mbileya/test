26a681f74c64e7529c356d214539898192227650
MB-R9\mbile <mbileya@hotmail.com>
2024-08-05 12:36:24 -0400

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        I did it MB is the man
       </p>
     </>
   )
